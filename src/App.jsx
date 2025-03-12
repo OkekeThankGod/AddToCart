1eb7f59ddf4990f98d901a6dad5b1f620bdcd410
OkekeThankGod <okekethankgod192@gmail.com.com>
2025-03-12 02:19:08 -1100

    Deploy React app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import Shop from './component/Shop'
 import { Cart } from "./component/Cart";
